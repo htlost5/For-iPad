c198846dd5c5c8afeb6a213eedeeb75e5724842b
htlost5 <142161677+htlost5@users.noreply.github.com>
2024-09-24 01:50:08 +0000

    all commit

---
+++ money2.py
@@ -0,0 +1,15 @@
+price = 100
+print(f'商品は{price}円です。')
+money = int(input("お金を投入してください："))
+if money >= price:
+    print("お金が投入されました。")
+    touch = input("購入しますか？ y/n :")
+    if touch == "y":
+        print("商品をお取りください。")
+        if money > price:
+            print(f'おつりは{money-price}円です。')
+        else:
+            pass
+    else:
+        print("購入を中止します。")
+        print(f'お金をお取りください。({money}円)')
